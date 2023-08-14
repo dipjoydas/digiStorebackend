ff144502a3a1340e2d3d7642f61a5bd96324023f
dipjoy <dipjoy488@gmail.com>
2023-08-14 22:23:43 +0600

    mongoose file update

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,12 +4,17 @@ process.env.MONGODBURI
 mongoose.connect(process.env.MONGODBURI,{
     useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 })
 
 
 
 
-// const db = mongoose.connection;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
 // // Define the schema
 // const postSchema = new mongoose.Schema({
